68d7920fa59d500c9e19ebec53154e6f991e10de
CyberVitexus <info@vitexsoftware.cz>
2026-05-18 03:55:14 +0200

    chore: update packaging configuration to use custom autoloader, add appstream validation, and improve version stamping in rules

---
+++ debian/autoload.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+require_once '/usr/share/php/Composer/InstalledVersions.php';
+require_once '/usr/share/php/Ease/autoload.php';
+
+spl_autoload_register(function (string $class): void {
+    $prefix = 'mServer\\';
+    if (str_starts_with($class, $prefix)) {
+        $file = '/usr/share/php/mServer/' . str_replace('\\', '/', substr($class, strlen($prefix))) . '.php';
+        if (file_exists($file)) {
+            require $file;
+        }
+    }
+});
+
+(function (): void {
+    $versions = [];
+    foreach (\Composer\InstalledVersions::getAllRawData() as $d) {
+        $versions = array_merge($versions, $d['versions'] ?? []);
+    }
+    $name    = 'unknown';
+    $version = '0.0.0';
+    $versions[$name] = ['pretty_version' => $version, 'version' => $version,
+        'reference' => null, 'type' => 'library', 'install_path' => __DIR__,
+        'aliases' => [], 'dev_requirement' => false];
+    \Composer\InstalledVersions::reload([
+        'root' => ['name' => $name, 'pretty_version' => $version, 'version' => $version,
+            'reference' => null, 'type' => 'library', 'install_path' => __DIR__,
+            'aliases' => [], 'dev' => false],
+        'versions' => $versions,
+    ]);
+})();
